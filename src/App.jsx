48eb561a730ffa6c13d24df06fa07e62aa1764fc
86jen <pas.jen@hotmail.com>
2026-05-15 18:02:40 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-
 export default function App() {
   return (
-    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
-      <h1>Op Zoek Naar De Buit</h1>
-      <p>De app werkt.</p>
+    <div style={{ padding: '40px', fontFamily: 'Arial' }}>
+      <h1>Test werkt</h1>
     </div>
   )
 }
